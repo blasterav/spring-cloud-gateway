e1b3dc88fa250797fc96c655f390b6daa5519b46
Aleksei Rygzynov <aleksei.ryg@ascendcorp.com>
2021-08-23 12:06:51 +0700

    [phoosop] Remove blockhound

--- src/main/java/com/phoosop/gateway/GatewayApplication.java
+++ src/main/java/com/phoosop/gateway/GatewayApplication.java
@@ -2,13 +2,12 @@ package com.phoosop.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		BlockHound.install();
+//		BlockHound.install();
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
